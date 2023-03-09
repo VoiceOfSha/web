60e0b10f110715067630bfdf053f5815b5ddaa5e
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-09 19:18:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'GreyMatters_Bot'
 
 #Ex https://Greymattersbot:ghp_147bkkabcdefgh@github.com/Greymattersbot/Mogenius
-os.system("git clone https://TamilanBotsZ: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@github.com/TamilanBotsZ/blaste okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://VoiceOfSha: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@github.com/VoiceOfSha/blaste okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
